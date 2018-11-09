f5d925d6bacc60e14fa5d232b0c6b0eb6542b653
sebastian <sebastian.matkovich@gmail.com>
2018-11-09 12:58:30 +0100

    wrote whole program for 1.6

--- Skeleton für Prog 1.6.py
+++ Skeleton für Prog 1.6.py
@@ -16,16 +16,26 @@ n=1000
 k=343
 ## Integral von f ueber das Intervall (a,b)
 i = lambda p: prior(p)*scipy.misc.comb ( n, k )*p**k*(1-p)**(n-k)
-j = lambda p: i / scipy.integrate.quad ( i, 0, 1 ) 
+u = lambda p: math.sin(math.pi*p)**2
+v = lambda p: u(p) / scipy.integrate.quad ( u, 0, 1 ) 
+j = lambda p: i(p) / scipy.integrate.quad ( i, 0, 1 ) 
 
 ## Teile das Intervall (a,b) in n "bins"
 x = numpy.linspace ( 0, 1, n )
-y = j(numpy.meshgrid(x))
+w = []
+for s in range (0, len(x)):
+  w.append(v(x[s]))
+y = []
+for t in range (0, len(x)):
+  y.append(j(x[t]))
 ## Plotten, siehe https://matplotlib.org/
 from matplotlib import pyplot as plt
 
 ## Plotte y versus x, als Linie, mit Label "label1"
-plt.plot ( y, x, label="label1"  ) 
+plt.plot ( y, x, label="polynom"  ) 
+plt.plot ( w, x, label="trigonometric"  ) 
+
+
 plt.title ( "titel" )
 
 ## Beschriftung der x-Achse
